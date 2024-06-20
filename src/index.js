c929127151c705ac15c21a1814bafce584553b41
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-06-20 18:08:05 +0300

    added a couple of strings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
     const user = req.query.name;
 
     if (!user) {
@@ -20,6 +20,19 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     res.send('POST /users');
 });
+
+app.get('/users:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
+
+app.get('/users:id/post/posts:postId', (req, res) => {
+    res.send(`User ID: ${req.params.id}, Post ID: ${res.params.postId}`);
+});
+
+app.get('/courses/:id', (req, res) => {
+    res.send(`Course ID: ${req.params.id}`);
+});
+
 
 app.listen({ port }, () => {
     console.log(`Example app listening on port ${port}`);
