b0f205c7ef84025cbb490594bf44eaf7e244b256
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-06-23 15:31:55 +0300

    Added github icon

--- src/index.js
+++ src/index.js
@@ -7,23 +7,23 @@ const port = 3000;
 
 await app.register(view, { engine: { pug } });
 const state = {
-    courses: [
-      {
-        id: 1,
-        title: 'JS: Массивы',
-        description: 'Курс про массивы в JavaScript',
-      },
-      {
-        id: 2,
-        title: 'JS: Функции',
-        description: 'Курс про функции в JavaScript',
-      },
-      {
-        id: 3,
-        title: 'Fastify',
-        description: 'Курс по фреймворку Fastify'
-      },
-    ],
+  courses: [
+    {
+      id: 1,
+      title: 'JS: Массивы',
+      description: 'Курс про массивы в JavaScript',
+    }, 
+    {
+      id: 2,
+      title: 'JS: Функции',
+      description: 'Курс про функции в JavaScript',
+    }, 
+    {
+      id: 3,
+      title: 'Fastify',
+      description: 'Курс по фреймворку Fastify'
+    },
+  ],
   };
 
 app.get('/', (req, res) => res.view('src/views/index'));
@@ -45,27 +45,25 @@ app.get('/users:id/post/posts:postId', (req, res) => {
 });
 
 app.get('/courses', (req, res) => {
-    const { term } = req.query;
-    let currentCurses = state.courses;
+  const { term } = req.query;
+  let currentCurses = state.courses;
 
-    if (term) {
-        currentCurses = state.courses.filter(({ title, description }) => (
-            title.toLowerCase().includes(term.toLowerCase()) || description.toLowerCase().includes(term.toLowerCase())
-        ));
-    }
+  if (term) {
+    currentCurses = state.courses.filter(({ title, description }) => (
+      title.toLowerCase().includes(term.toLowerCase()) || description.toLowerCase().includes(term.toLowerCase())
+    ));
+  }
 
-    const data = { term, courses: currentCurses };
-
-    res.view('src/views/courses/index.pug', data);
-
+  const data = { term, courses: currentCurses };
+  res.view('src/views/courses/index.pug', data);
 });
 
 app.get('/courses/:id', (req, res) => {
-    const course = state.courses.find(({id}) => id === Number(req.params.id));
-    res.view('src/views/courses/show.pug', { course });
+  const course = state.courses.find(({id}) => id === Number(req.params.id));
+  res.view('src/views/courses/show.pug', { course });
 });
 
 
 app.listen({ port }, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
