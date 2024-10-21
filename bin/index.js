f946a131aa7213e27333312fe6923cba78bb292a
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-10-21 20:44:50 +0300

    add

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,8 @@
 import build from '../src/index.js';
-import 'dotenv/config';
 
 const app = await build();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
