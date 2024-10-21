7ba6eeb01e92b352a38451a0161b1772f43497d4
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-10-21 20:49:34 +0300

    fixed

--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,10 @@ import build from '../src/index.js';
 
 const app = await build();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
-app.listen({ port }, () => {
+
+app.listen({ host, port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
