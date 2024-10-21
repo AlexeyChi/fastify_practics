e04d60123999f8204e78a0326f9879f7df9ac2c1
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-10-21 20:12:23 +0300

    added

--- bin/index.js
+++ bin/index.js
@@ -4,8 +4,6 @@ const app = await build();
 
 const port = process.env.PORT || 3000;
 
-console.log(process.env.PORT);
-
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
